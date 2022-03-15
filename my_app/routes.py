7503771c942dcbfc8f593ac24e5ce7c0d6b357ab
jacksor <jacksorjacksor@pm.me>
2022-03-15 23:53:51 +0000

    Added some specs

--- my_app/routes.py
+++ my_app/routes.py
@@ -4,6 +4,50 @@ from flask import render_template, redirect, url_for, jsonify
 import json
 from sqlalchemy.sql import func
 
+"""
+REQUIREMENTS
+So what does this actual need?
+- This will PRIMARILY be for ONE student's details.
+-- notable exception is for introduction of class ranking
+
+- Student ID should come from LOGGED IN USER
+-- While in dev can have a default "user_id==1"
+
+FEATURES REQUIRED
+- Assessment results view [ list view ]
+-- Shows questions and gives breakdown of average/total score so far
+-- Should be able to click on a Question to get a Question view
+
+- Module results view [ list view ]
+-- Shows questions and gives breakdown of average/total score so far
+-- Should be able to click on an Assessment to go to an Assessment view
+
+- Course results view [ list view ]
+-- Shows all modules so far
+-- Shows predicted grade
+
+- Question view [ detail view ]
+-- Unsure on what this would show but worth considering.
+
+DETAILS REQUIRED
+- Assessment results view [ list view ]
+-- Calculated fields:
+
+--# PRIMARY
+--- Marks achieved
+--- Marks possible
+
+--# SECONDARY
+--- Answers submitted
+--- Answers possible
+---- If questions have categories then could have a category breakdown
+
+----- FUNCTIONALITY: 
+must be able to export all data as .csv
+
+"""
+
+
 # For more complicated things we should use numpy/pandas.
 
 
